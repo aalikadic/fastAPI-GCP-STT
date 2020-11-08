fbc79b7acd8508b2c265d710780ac8f1f01d32df
Amar Alikadic <alikadic.amar@gmail.com>
2020-11-08 20:28:32 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
 from fastapi import FastAPI
+import configparser
 
+config_file = configparser.ConfigParser()
+config_file.read('config.ini')
 app = FastAPI()
 
 #domain where this api is hosted for example : localhost:5000/docs to see swagger documentation automagically generated.
