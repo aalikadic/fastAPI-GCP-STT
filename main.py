281be26989a1fae29c56451edb63476620b9f506
Amar Alikadic <alikadic.amar@gmail.com>
2020-11-08 20:33:19 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,12 @@
 from fastapi import FastAPI
 import configparser
+import os
 
 config_file = configparser.ConfigParser()
 config_file.read('config.ini')
+
+API = str(config_file.get('api','name'))
+os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = API
 app = FastAPI()
 
 #domain where this api is hosted for example : localhost:5000/docs to see swagger documentation automagically generated.
