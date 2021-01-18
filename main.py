c02de3a1f8e07da08926ab4ce4629c04eb30e03b
Amar Alikadic <alikadic.amar@gmail.com>
2021-01-18 21:40:10 +0100

    Update main.py

--- main.py
+++ main.py
@@ -30,7 +30,10 @@ app = FastAPI(
 
     )
 app.add_middleware(CORSMiddleware,
-    allow_origins=["*"])
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],)
 """HEALTH CHECK ENDPOINT, RETURNS A SHORT MESSAGE"""
 @app.get("/")
 def home():
